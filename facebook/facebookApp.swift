824b8f05c3f91c5f07fc9aa6a2547aa37f228878
Francesco Paciello <kekkopacio@gmail.com>
2021-05-28 12:55:33 +0200

    Initial Commit

---
+++ facebook/facebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  facebookApp.swift
+//  facebook
+//
+//  Created by Kekko Paciello on 28/05/21.
+//
+
+import SwiftUI
+
+@main
+struct facebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
